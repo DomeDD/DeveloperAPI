d3d5235ab02064a4d43426390e4019abd94529be
DomeDD <59269dd@gmail.com>
2017-11-08 18:37:57 +0100

    Create BookBuilder.java

---
+++ BookBuilder.java
@@ -0,0 +1,34 @@
+class BookBuilder extends ItemBuilder {
+	
+	/**
+	 * constructor
+	 * @param itemStack itemstack
+	 */
+	BookBuilder(ItemStack itemStack);
+	
+	/**
+	 * constructor
+	 * @param amount amount
+	 */
+	BookBuilder(int amount);
+	
+	/**
+	 * @param name author's name
+	 */
+	BookBuilder setAuthor(String name);
+  
+  /**
+	 * @param content page content
+	 */
+	BookBuilder addPage(String content);
+  
+  /**
+	 * @param contents list of page contents
+	 */
+	BookBuilder addPages(List<String> contents);
+  
+  /**
+	 * @return amount of pages
+	 */
+	BookBuilder getPageCount();
+}
