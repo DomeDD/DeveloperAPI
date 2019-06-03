adcf2ee8ef12f3a53e2e13a83e3e4a250f14c95f
DomeDD <59269dd@gmail.com>
2019-06-03 20:15:20 +0200

    update to v1.8

--- VersionChecker.java
+++ VersionChecker.java
@@ -6,6 +6,6 @@ class VersionChecker {
 	BukkitVersion getBukkitVersion();
 	
 	enum BukkitVersion {
-		v1_8_R1, v1_8_R2, v1_8_R3, v1_9_R1, v1_9_R2, v1_10_R1, v1_11_R1, v1_12_R1; 
+		v1_8_R1, v1_8_R2, v1_8_R3, v1_9_R1, v1_9_R2, v1_10_R1, v1_11_R1, v1_12_R1, v1_13_R1, v1_13_R2, v1_14_R1; 
 	}
 }
